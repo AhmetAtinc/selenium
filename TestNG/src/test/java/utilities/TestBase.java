65977ba7bdd3efac9ac63e46140cb2c711122466
AhmetAtinc <94438739+AhmetAtinc@users.noreply.github.com>
2022-07-01 22:25:42 +0300

    Q

--- TestNG/src/test/java/utilities/TestBase.java
+++ TestNG/src/test/java/utilities/TestBase.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
